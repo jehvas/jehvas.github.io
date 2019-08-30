23688357b25599628dcceb6a3859c547a4065640
Jens Hvass <jhj@mindkey.com>
2019-08-30 23:31:26 +0200

    Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, Output} from '@angular/core';
+import {analyzeAndValidateNgModules} from "@angular/compiler";
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,11 @@ import {Component, Output} from '@angular/core';
 export class AppComponent {
   title = 'app';
   all_numbers = [];
-  selected = Array(100).fill(false);
+  selected = Array(90).fill(false);
   random_num = 0;
+
   constructor() {
-    this.all_numbers = Array(100).fill(0).map((x, i) => i + 1);
+    this.all_numbers = Array(90).fill(0).map((x, i) => i + 1);
   }
 
   onClicked(num: any) {
@@ -19,6 +21,13 @@ export class AppComponent {
   }
 
   random_number() {
-    this.random_num = Math.round(Math.random()*100);
+    const available_numbers = this.selected.map((x, i) => {
+      if (!x) {
+        return i + 1;
+      }
+    }).filter(x => x !== undefined);
+    const random_index = Math.floor(Math.random() * available_numbers.length);
+    this.random_num = available_numbers[random_index];
+    this.selected[this.random_num - 1] = true;
   }
 }
