dbdbd3075fc6a30e9bc65b9fdf4da7ae1a47c616
Jens Hvass <jhj@mindkey.com>
2020-04-10 18:28:48 +0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import {Component, Output} from '@angular/core';
-import {analyzeAndValidateNgModules} from "@angular/compiler";
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
